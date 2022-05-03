df8f9b90a3bd86900ee1887915080b379cf8f8bb
unknown <leluan2047@gmail.com>
2022-05-03 19:45:36 +0700

    L up

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import "./Home.scss";
 import HeroSection from '../../components/Herosection/HeroSection';
-import MainPage from '../../components/MainPage/MainContent';
-import "./Home.css";
+import MainPage from '../../components/MainPage/MainPage';
+
 function Home() {
   return (
     <div className='home-container'>
